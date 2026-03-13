34451192bb262b9db1f3d0ad56bfb4f4b637ffd2
darabel <baidammee@gmail.com>
2026-03-13 03:24:55 +0100

    Added Week 2 Day 11 Smart Calculator with functions

---
+++ week2/Day11/day11_assignment.py
@@ -0,0 +1,43 @@
+# This code is to generate a simple calculator using functions
+def add(a, b):
+    return a + b
+    
+def substract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: Division by zero is not allowed."
+
+print("===== SMART CALCULATOR =====")
+print("Select operation:")
+print("1. Add")
+print("2. Substract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = input("Enter choice (1/2/3/4): ")
+if choice in ['1', '2', '3', '4']:
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == '1':
+        print(f"{num1} + {num2} = {add(num1, num2)}")
+    elif choice == '2':
+        print(f"{num1} - {num2} = {substract(num1, num2)}")
+    elif choice == '3':
+        print(f"{num1} * {num2} = {multiply(num1, num2)}")
+    elif choice == '4':
+        print(f"{num1} / {num2} = {divide(num1, num2)}")
+else:
+    print("Invalid input. Please select a valid operation.")
+
+
+print(f"\nThank you for using the Simple Calculator!")
+
+
---
+++ week2/Day11/day11_exercise.py
@@ -0,0 +1,9 @@
+# This code is showing the use of functions
+def greet(name):
+    print(f"Hello, {name}! Welcome to the Python 90 Days Challenge!")
+greet("Alice")
+
+def add(a, b):
+    return a + b
+result = add(5, 3)
+print(f"The sum of 5 and 3 is: {result}")
