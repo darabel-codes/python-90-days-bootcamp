8bf8ae891f1028496c8a663a75689ecf058652ff
darabel <baidammee@gmail.com>
2026-05-15 10:54:57 +0100

    Day 66 Banner Grabber added successfully

---
+++ week9/Day66/day66_banner_grabber.py
@@ -0,0 +1,32 @@
+import socket
+
+print("===== BANNER GRABBER =====")
+
+target = input("Enter the target IP address or domain name: ")
+port = int(input("Enter the port number to grab the banner from: "))
+
+try:
+    
+    scanner = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    scanner.settimeout(3)
+    
+    scanner.connect((target, port))
+    banner = scanner.recv(1024)
+    
+    print("\n[+] Banner Received:")
+    print(banner.decode().strip())
+    
+    scanner.close()
+    
+except socket.timeout:
+    print("\n[-] Connection timed out. No banner received.")
+
+except ConnectionRefusedError:
+    print("\n[-] Connection refused. The port may be closed.")
+
+except socket.gaierror:
+    print("\n[-] Hostname invalid.")
+
+except Exception as e:
+    print(f"\n[-] An error occurred: {e}")
